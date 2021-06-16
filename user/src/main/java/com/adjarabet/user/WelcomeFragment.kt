908ad565692ccb33797da744a847753820fb5d8a
vakhokurdadze <vakhokurdadze@gmail.com>
2021-06-16 17:29:08 +0400

    who starts selector added on welcome screen

--- user/src/main/java/com/adjarabet/user/WelcomeFragment.kt
+++ user/src/main/java/com/adjarabet/user/WelcomeFragment.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.fragment.app.Fragment
 import com.adjarabet.basemodule.Constants
+import com.adjarabet.basemodule.SnackbarProvider
 import kotlinx.android.synthetic.main.fragment_match.view.*
 import kotlinx.android.synthetic.main.fragment_welcome.view.*
 import ru.terrakok.cicerone.Router
@@ -27,11 +29,35 @@ class WelcomeFragment : Fragment(){
         router = (activity?.application as WordGameApplication).cicerone.router
 
         welcomeView.start.setOnClickListener {
-            Intent().also { intent ->
-                intent.action = Constants.ACTION_START_BOT_SERVICE
-                activity?.sendBroadcast(intent)
+
+            val whoStartsId = welcomeView.whoStartsRadioButtonGroup.checkedRadioButtonId
+            if (whoStartsId == -1) {
+
+                SnackbarProvider.newSnackBar(
+                    requireContext(),
+                    resources.getString(R.string.whoStartsNotSelectedErrorHeader),
+                    resources.getString(R.string.whoStartsNotSelectedErrorDesc),
+                    welcomeView.welcomeFragmentRoot,
+                    150
+                ).show()
+                return@setOnClickListener
+            } else {
+                val whoStartsSelected = welcomeView.findViewById<RadioButton>(
+                    whoStartsId
+                )
+                if (whoStartsSelected.id == R.id.botStartsRadioButton) {
+
+
+                } else {
+
+                }
+                Intent().also { intent ->
+                    intent.action = Constants.ACTION_START_BOT_SERVICE
+                    activity?.sendBroadcast(intent)
+                }
+                router.navigateTo(SimpleScreen(Routes.MATCH_SCREEN))
             }
-            router.navigateTo(SimpleScreen(Routes.MATCH_SCREEN))
+
         }
         return welcomeView
     }
